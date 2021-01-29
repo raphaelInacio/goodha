7a2bf6c2afdc0e4fd02d0b2a4034b8e7fde2fc57
Raphael Inacio Silva <raphaelinacio@MacBook-Pro-de-Raphael.local>
2021-01-29 08:53:24 -0300

    Dependencias GCP

--- src/test/java/br/com/goodha/GodhaApplicationTests.java
+++ src/test/java/br/com/goodha/GodhaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GodhaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
